353ef554a7226928b70f174d0fbd18493c4ccca2
Danny Tuppeny <danny@tuppeny.com>
2015-06-06 20:02:49 +0100

    Fix building of source to actually work!

--- JsonBuilder.Tests/CSharpFileBuilderTests.cs
+++ JsonBuilder.Tests/CSharpFileBuilderTests.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
-namespace JsonBuilder.Tests
+namespace DartVS.DartAnalysis.JsonBuilder.Tests
 {
     public class CSharpFileBuilderTests
     {
--- JsonBuilder/CSharpFileBuilder.cs
+++ JsonBuilder/CSharpFileBuilder.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.CodeAnalysis.CSharp;
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Formatting;
 using Microsoft.CodeAnalysis.MSBuild;
@@ -7,29 +8,34 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace DartVS.DartAnalysis.JsonBuilder
 {
 	public class CSharpFileBuilder
 	{
-		NamespaceDeclarationSyntax @namespace;
+		readonly string @namespace;
+		readonly List<CSharpClass> classes = new List<CSharpClass>();
 
 		public CSharpFileBuilder(string @namespace) {
-			this.@namespace = SyntaxFactory.NamespaceDeclaration(SyntaxFactory.IdentifierName(@namespace));
+			this.@namespace = @namespace;
         }
 
 		public CSharpClass AddClass(string name)
 		{
-			var @class = SyntaxFactory.ClassDeclaration(name);
-            this.@namespace = this.@namespace.AddMembers(@class);
-			return new CSharpClass(@class);
+			var @class = new CSharpClass(name);
+			classes.Add(@class);
+			return @class;
         }
 
 		public string GetContents()
 		{
+			var document =
+				SyntaxFactory
+					.NamespaceDeclaration(SyntaxFactory.IdentifierName(@namespace))
+					.WithMembers(GetClasses());
+
 			var workspace = MSBuildWorkspace.Create();
-			var formattedResult = Formatter.Format(@namespace, workspace);
+			var formattedResult = Formatter.Format(document, workspace);
 
 			var output = new StringBuilder();
 			using (var sw = new StringWriter(output))
@@ -37,22 +43,41 @@ namespace DartVS.DartAnalysis.JsonBuilder
 
 			return output.ToString();
 		}
+		
+		SyntaxList<MemberDeclarationSyntax> GetClasses()
+		{
+			return SyntaxFactory.List(
+				classes.Select(c => c.GetClass())
+			);
+		}
 	}
 
 	public class CSharpClass
 	{
-		ClassDeclarationSyntax @class;
+		readonly string name;
+		readonly List<Tuple<Type, string>> properties = new List<Tuple<Type, string>>();
 
-		public CSharpClass(ClassDeclarationSyntax @class)
+		public CSharpClass(string name)
 		{
-			this.@class = @class;
+			this.name = name;
 		}
 
 		public void AddProperty<T>(string name)
 		{
-			var property = SyntaxFactory.PropertyDeclaration(SyntaxFactory.ParseTypeName(typeof(T).FullName), name);
+			properties.Add(Tuple.Create(typeof(T), name));
+		}
 
-			@class = @class.AddMembers(property);
+		public MemberDeclarationSyntax GetClass()
+		{
+			return SyntaxFactory
+				.ClassDeclaration(name)
+				.WithMembers(GetProperties());
+		}
+		SyntaxList<MemberDeclarationSyntax> GetProperties()
+		{
+			return SyntaxFactory.List<MemberDeclarationSyntax>(
+				properties.Select(p => SyntaxFactory.PropertyDeclaration(SyntaxFactory.ParseTypeName(p.Item1.FullName), p.Item2))
+			);
 		}
 	}
 }
