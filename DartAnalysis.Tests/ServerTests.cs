f457056dc09bd0f6ec7b7449593fb4e7c9a96e88
Danny Tuppeny <danny@tuppeny.com>
2015-06-06 17:08:12 +0100

    Added empty service and failing stub test.

---
+++ DartAnalysis.Tests/ServerTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DartAnalysis.Tests
+{
+    public class ServerTests
+    {
+		[Fact]
+		public async Task ServerVersion()
+		{
+			var service = new DartAnalysisService(new DirectoryInfo(@"M:\Apps\dart-sdk"));
+			var version = await service.GetVersion();
+			Assert.Equal(new Version("1.2.3.4"), version);
+		}
+    }
+}
--- DartAnalysis/DartAnalysisService.cs
+++ DartAnalysis/DartAnalysisService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,20 @@ namespace DartAnalysis
 {
     public class DartAnalysisService
     {
+		readonly DirectoryInfo SdkLocation;
+		readonly FileInfo DartVmLocation;
+		readonly FileInfo AnalysisServerSnapshotLocation;
+
+		public DartAnalysisService(DirectoryInfo sdkLocation)
+		{
+			this.SdkLocation = sdkLocation;
+			this.DartVmLocation = new FileInfo(Path.Combine(sdkLocation.FullName, @"bin\dart.exe"));
+			this.AnalysisServerSnapshotLocation = null;
+		}
+
+		public Task<Version> GetVersion()
+		{
+			throw new NotImplementedException();
+		}
     }
 }
