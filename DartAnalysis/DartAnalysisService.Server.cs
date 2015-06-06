f8cf21b02858c142307fd629ce0ecf73e4edc7f2
Danny Tuppeny <danny@tuppeny.com>
2015-06-06 17:33:38 +0100

    Write Analysis Server snapshot to disk to pass to VM.

---
+++ DartAnalysis/DartAnalysisService.Server.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DartVS.DartAnalysis
+{
+    public partial class DartAnalysisService
+    {
+		public Task<Version> GetVersion()
+		{
+			throw new NotImplementedException();
+		}
+    }
+}
--- DartAnalysis/DartAnalysisService.cs
+++ DartAnalysis/DartAnalysisService.cs
@@ -1,28 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
+using System.Reflection;
 
-namespace DartAnalysis
+namespace DartVS.DartAnalysis
 {
-    public class DartAnalysisService
+    public partial class DartAnalysisService
     {
 		readonly DirectoryInfo SdkLocation;
 		readonly FileInfo DartVmLocation;
-		readonly FileInfo AnalysisServerSnapshotLocation;
+		static readonly FileInfo AnalysisServerSnapshotLocation;
+		const string AnalysisServerFilename = "analysis_server.dart.snapshot";
+
+		#region Static setup
+
+		static DartAnalysisService()
+		{
+			AnalysisServerSnapshotLocation = WriteAnalysisServerSnapshot();
+		}
+
+		static FileInfo WriteAnalysisServerSnapshot()
+		{
+			var asm = Assembly.GetExecutingAssembly();
+			var snapshotStream = asm.GetManifestResourceStream(string.Format("{0}.{1}", typeof(DartAnalysisService).Namespace, AnalysisServerFilename));
+
+			var tempFolder = Path.Combine(Path.GetTempPath(), "DartAnalysis.NET", Path.GetRandomFileName());
+			var file = Path.Combine(tempFolder, AnalysisServerFilename);
+
+			Directory.CreateDirectory(tempFolder);
+			using (var fileStream = File.Create(file))
+				snapshotStream.CopyTo(fileStream);
+
+			return new FileInfo(file);
+		}
+
+		#endregion
 
 		public DartAnalysisService(DirectoryInfo sdkLocation)
 		{
 			this.SdkLocation = sdkLocation;
 			this.DartVmLocation = new FileInfo(Path.Combine(sdkLocation.FullName, @"bin\dart.exe"));
-			this.AnalysisServerSnapshotLocation = null;
 		}
-
-		public Task<Version> GetVersion()
-		{
-			throw new NotImplementedException();
-		}
-    }
+	}
 }
