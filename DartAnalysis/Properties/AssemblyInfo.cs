0c94d8b7574152286ba109e994f6709392a2ad7c
Danny Tuppeny <danny@tuppeny.com>
2015-06-06 16:47:57 +0100

    Empty project for service.

---
+++ DartAnalysis/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("DartVS DartAnalysis Service")]
+[assembly: AssemblyDescription(".NET wrapper over the Dart Analysis Server.")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
