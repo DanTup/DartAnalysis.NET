ee4678fe1ac05c9c627dcfd33ccfacaad1a103ca
Danny Tuppeny <danny@tuppeny.com>
2015-06-07 17:53:39 +0100

    Wire up creation of (empty) Request classes.

--- JsonBuilder/JsonBuilder.cs
+++ JsonBuilder/JsonBuilder.cs
@@ -1,10 +1,14 @@
-﻿using System.IO;
+﻿using System;
+using System.Linq;
+using System.IO;
 using System.Xml.Linq;
 
 namespace DartVS.DartAnalysis.JsonBuilder
 {
 	class JsonBuilder
 	{
+		CSharpFileBuilder builder = new CSharpFileBuilder("DartVS.DartAnalysis");
+
 		public JsonBuilder(FileInfo apiDoc)
 		{
 			VisitChildren(XDocument.Load(apiDoc.FullName));
@@ -26,6 +30,21 @@ namespace DartVS.DartAnalysis.JsonBuilder
 
 		void VisitRequest(XElement element)
 		{
+			var domainName = element.Ancestors("domain").First().Attribute("name").Value;
+			var methodName = element.Attribute("method").Value;
+			var className = GenerateName(domainName, methodName);
+			var doc = element.Element("p").Value;
+			builder.AddClass(className, doc);
+		}
+
+		string GenerateName(params string[] parts)
+		{
+			return string.Join("", parts.Select(p => char.ToUpper(p[0]) + p.Substring(1)));
+		}
+
+		public string GetContents()
+		{
+			return builder.GetContents();
 		}
 	}
 }
--- JsonBuilder/Program.cs
+++ JsonBuilder/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -19,6 +20,8 @@ namespace DartVS.DartAnalysis.JsonBuilder
 		{
 			var builder = new JsonBuilder(new FileInfo(ApiDoc));
 
+			var output = builder.GetContents();
+			Debugger.Break();
 		}		
 	}
 }
