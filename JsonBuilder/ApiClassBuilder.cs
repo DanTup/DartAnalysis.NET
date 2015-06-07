4d43d5663ceda42fe63c1efcaa6f3c61c083d605
Danny Tuppeny <danny@tuppeny.com>
2015-06-07 18:52:44 +0100

    Extract some logic around how to build API classes into its own class to keep the visitor class straight forward.

---
+++ JsonBuilder/ApiClassBuilder.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DartVS.DartAnalysis.JsonBuilder
+{
+	class ApiClassBuilder
+	{
+		CSharpFileBuilder builder = new CSharpFileBuilder("DartVS.DartAnalysis");
+
+		public void BuildRequestClass(XElement requestElement)
+		{
+			var domainName = requestElement.Ancestors("domain").First().Attribute("name").Value;
+			var methodName = requestElement.Attribute("method").Value;
+			var doc = requestElement.Element("p").Value;
+
+			var requestClass = builder.AddClass(Name(domainName, methodName, "Request"), doc);
+			if (requestElement.Element("params") != null)
+			{
+				foreach (var field in requestElement.Elements("field"))
+					// TODO: Fix type
+					requestClass.AddProperty<string>(Name(field.Attribute("name").Value), field.Element("p").Value);
+			}
+		}
+		
+		public void BuildResponseClass(XElement requestElement)
+		{
+			// If we don't return a result, we don't need to build a class.
+			if (requestElement.Element("result") == null)
+				return;
+
+			var domainName = requestElement.Ancestors("domain").First().Attribute("name").Value;
+			var methodName = requestElement.Attribute("method").Value;
+			var doc = requestElement.Element("p").Value;
+
+			var responseClass = builder.AddClass(Name(domainName, methodName, "Response"), doc);
+			foreach (var field in requestElement.Element("result").Elements("field"))
+				// TODO: Fix type
+				responseClass.AddProperty<string>(Name(field.Attribute("name").Value), field.Element("p").Value);
+		}
+
+		string Name(params string[] parts)
+		{
+			return string.Join("", parts.Select(p => char.ToUpper(p[0]) + p.Substring(1)));
+		}
+
+		public string GetContents()
+		{
+			return builder.GetContents();
+		}
+	}
+}
---
+++ JsonBuilder/ApiDocVisitor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Xml.Linq;
+
+namespace DartVS.DartAnalysis.JsonBuilder
+{
+	class ApiClassVisitor
+	{
+		ApiClassBuilder classBuilder = new ApiClassBuilder();
+
+		public ApiClassVisitor(FileInfo apiDoc)
+		{
+			VisitChildren(XDocument.Load(apiDoc.FullName));
+		}
+
+		void VisitChildren(XContainer parent)
+		{
+			foreach (var element in parent.Elements())
+				Visit(element);
+		}
+
+		void Visit(XElement element)
+		{
+			if (element.Name == "request")
+				VisitRequest(element);
+			else
+				VisitChildren(element);
+		}
+
+		void VisitRequest(XElement element)
+		{
+			classBuilder.BuildRequestClass(element);
+			classBuilder.BuildResponseClass(element);			
+		}		
+
+		public string GetContents()
+		{
+			return classBuilder.GetContents();
+		}
+	}
+}
